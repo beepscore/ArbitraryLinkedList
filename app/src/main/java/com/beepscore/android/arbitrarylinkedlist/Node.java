f2fd35ea63388c79373b8386fa3b4d1467922241
beepscore <support@beepscore.com>
2015-05-15 20:37:09 -0700

    Add Node.java

---
+++ app/src/main/java/com/beepscore/android/arbitrarylinkedlist/Node.java
@@ -0,0 +1,11 @@
+package com.beepscore.android.arbitrarylinkedlist;
+
+/**
+ * Created by stevebaker on 5/15/15.
+ */
+public class Node {
+    String value;
+    Node next;
+    Node arbitrary;
+}
+
