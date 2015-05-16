b1f923f8ad75454e44685ae3d4aa48a1cc5b5612
beepscore <support@beepscore.com>
2015-05-16 16:44:14 -0700

    Add ArbitraryLinkedListManagerTest and testArbitraryLinkedListManager

---
+++ app/src/androidTest/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManagerTest.java
@@ -0,0 +1,16 @@
+package com.beepscore.android.arbitrarylinkedlist;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by stevebaker on 5/16/15.
+ */
+public class ArbitraryLinkedListManagerTest extends TestCase {
+
+    public void testArbitraryLinkedListManager() {
+        ArbitraryLinkedListManager listManager = new ArbitraryLinkedListManager();
+        assertNotNull(listManager);
+    }
+
+}
+
