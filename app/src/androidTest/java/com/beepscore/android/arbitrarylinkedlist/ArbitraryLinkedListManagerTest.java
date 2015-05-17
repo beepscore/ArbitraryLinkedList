fa3b72df174c866c9321dd91dbb9b454252e319d
beepscore <support@beepscore.com>
2015-05-16 17:27:43 -0700

    In ArbitraryLinkedListManager add property clonedNodeSet. In ArbitraryLinkedListManagerTest add testClonedNodeSet.

--- app/src/androidTest/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManagerTest.java
+++ app/src/androidTest/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManagerTest.java
@@ -2,6 +2,8 @@ package com.beepscore.android.arbitrarylinkedlist;
 
 import junit.framework.TestCase;
 
+import java.util.HashSet;
+
 /**
  * Created by stevebaker on 5/16/15.
  */
@@ -12,5 +14,14 @@ public class ArbitraryLinkedListManagerTest extends TestCase {
         assertNotNull(listManager);
     }
 
+    public void testClonedNodeSet() {
+        ArbitraryLinkedListManager listManager = new ArbitraryLinkedListManager();
+        listManager.clonedNodeSet = new HashSet<Node>();
+
+        Node curly = new Node();
+        assertFalse(listManager.clonedNodeSet.contains(curly));
+        listManager.clonedNodeSet.add(curly);
+        assertTrue(listManager.clonedNodeSet.contains(curly));
+    }
 }
 
--- app/src/main/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManager.java
+++ app/src/main/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManager.java
@@ -1,8 +1,17 @@
 package com.beepscore.android.arbitrarylinkedlist;
 
+import java.util.Set;
+
 /**
  * Created by stevebaker on 5/15/15.
  * Manages an Arbitrary Linked List of Node objects.
  */
 public class ArbitraryLinkedListManager {
+
+    /**
+     * Keeps inventory of cloned nodes.
+     * Can check to avoid re-cloning a node that already exists.
+     */
+    public Set<Node> clonedNodeSet;
+
 }
