1c88aa7b82928206d8e3a27b9428e86411628afb
beepscore <support@beepscore.com>
2015-05-17 03:09:32 -0700

    In ArbitraryLinkedListManager delete clonedNodeSet, add correspondingNodes. Add methods cloneList, cloneOriginalProperties. cloneOriginalNext, cloneOriginalArbitrary isLastNode.
    In ArbitraryLinkedListManager delete testClonedNodeSet. Add tests for several new methods.
    TODO: Test clone node arbitrary link is being set correctly.

--- app/src/androidTest/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManagerTest.java
+++ app/src/androidTest/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManagerTest.java
@@ -1,27 +1,124 @@
 package com.beepscore.android.arbitrarylinkedlist;
 
+import android.util.Log;
+
 import junit.framework.TestCase;
 
-import java.util.HashSet;
+import java.util.HashMap;
 
 /**
  * Created by stevebaker on 5/16/15.
  */
 public class ArbitraryLinkedListManagerTest extends TestCase {
 
+    ArbitraryLinkedListManager listManager;
+
+    public void setUp() {
+        listManager = new ArbitraryLinkedListManager();
+    }
+
+    public Node getSampleListArbitraryLinksToStart() {
+        Node a = new Node();
+        Node b = new Node();
+        Node c = new Node();
+        Node d = new Node();
+
+        a.value = "a";
+        b.value = "b";
+        c.value = "c";
+        d.value = "d";
+
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        d.next = null;
+
+        c.arbitrary = a;
+
+        return a;
+    }
+
+    /*
+     * Check test data
+     */
+    public void testGetSampleListArbitraryLinksToStart() {
+        Node startNode = getSampleListArbitraryLinksToStart();
+        assertNotNull(startNode.next);
+        assertNotNull(startNode.next.next);
+        assertNotNull(startNode.next.next.next);
+        assertNull(startNode.next.next.next.next);
+
+        assertEquals("a", startNode.value);
+        assertEquals("b", startNode.next.value);
+    }
+
     public void testArbitraryLinkedListManager() {
-        ArbitraryLinkedListManager listManager = new ArbitraryLinkedListManager();
         assertNotNull(listManager);
     }
 
-    public void testClonedNodeSet() {
-        ArbitraryLinkedListManager listManager = new ArbitraryLinkedListManager();
-        listManager.clonedNodeSet = new HashSet<Node>();
+    public void testCorrespondingNodes() {
+        listManager.correspondingNodes = new HashMap<Node, Node>();
 
-        Node curly = new Node();
-        assertFalse(listManager.clonedNodeSet.contains(curly));
-        listManager.clonedNodeSet.add(curly);
-        assertTrue(listManager.clonedNodeSet.contains(curly));
+        Node originalNode = new Node();
+        listManager.correspondingNodes.put(originalNode, null);
+        // get value for key
+        assertNull(listManager.correspondingNodes.get(originalNode));
+
+        Node cloneNode = new Node();
+        listManager.correspondingNodes.put(originalNode, cloneNode);
+        assertEquals(cloneNode, listManager.correspondingNodes.get(originalNode));
     }
+
+    public void testIsLastNode() {
+        Node curly = new Node();
+        curly.next = null;
+        assertTrue(listManager.isLastNode(curly));
+        Node larry = new Node();
+        curly.next = larry;
+        assertFalse(listManager.isLastNode(curly));
+        assertTrue(listManager.isLastNode(larry));
+    }
+
+    public void testCloneOriginalNext() {
+        Node a = new Node();
+        Node b = new Node();
+        Node cloneA = new Node();
+
+        a.value = "a";
+        b.value = "b";
+        a.next = b;
+
+        listManager.correspondingNodes = new HashMap<Node, Node>();
+        listManager.cloneOriginalNext(a, cloneA);
+        assertNotNull(cloneA.next);
+
+        // cloneOriginalNext doesn't set cloned next node's value
+        assertNull(cloneA.next.value);
+    }
+
+
+    public void testCloneList() {
+        Node startNode = getSampleListArbitraryLinksToStart();
+        Node cloneStartNode = listManager.cloneList(startNode);
+
+        Log.d("correspondingNodes",
+                listManager.correspondingNodes.toString());
+
+        assertEquals(cloneStartNode,
+                listManager.correspondingNodes.get(startNode));
+
+        assertEquals(4, listManager.correspondingNodes.size());
+
+        assertNotNull(cloneStartNode.next);
+        assertNotNull(cloneStartNode.next.next);
+        assertNotNull(cloneStartNode.next.next.next);
+        assertNull(cloneStartNode.next.next.next.next);
+
+        assertEquals("a", cloneStartNode.value);
+        assertEquals("b", cloneStartNode.next.value);
+        assertEquals("c", cloneStartNode.next.next.value);
+        assertEquals("d", cloneStartNode.next.next.next.value);
+    }
+
 }
 
--- app/src/main/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManager.java
+++ app/src/main/java/com/beepscore/android/arbitrarylinkedlist/ArbitraryLinkedListManager.java
@@ -1,6 +1,6 @@
 package com.beepscore.android.arbitrarylinkedlist;
 
-import java.util.Set;
+import java.util.HashMap;
 
 /**
  * Created by stevebaker on 5/15/15.
@@ -9,9 +9,81 @@ import java.util.Set;
 public class ArbitraryLinkedListManager {
 
     /**
-     * Keeps inventory of cloned nodes.
+     * Maps original node to cloned node
      * Can check to avoid re-cloning a node that already exists.
      */
-    public Set<Node> clonedNodeSet;
+    // Use Map interface, Dictionary is obsolete.
+    // http://docs.oracle.com/javase/7/docs/api/java/util/Dictionary.html
+    // Also HashMap allows value null, Dictionary HashTable doesn't
+    public HashMap<Node, Node> correspondingNodes;
+
+    /**
+     * Clones list from start node through last node inclusive.
+     * At each original node, make a clone and record them in correspondingNodes.
+     * @param startNode is the starting node in the original list
+     * @return the starting node in the clone list
+     */
+    public Node cloneList(Node startNode) {
+
+        if (startNode == null) {
+            return null;
+        }
+
+        correspondingNodes = new HashMap<Node, Node>();
+
+        Node originalNode = startNode;
+
+        while (originalNode != null) {
+
+            Node cloneNode = null;
+            if (correspondingNodes.get(originalNode) != null) {
+                // The corresponding cloneNode already exists. Use it.
+                cloneNode = correspondingNodes.get(originalNode);
+            } else {
+                cloneNode = new Node();
+                // put cloneNode in correspondingNodes immediately to
+                // prevent accidental duplication later
+                correspondingNodes.put(originalNode, cloneNode);
+            }
+
+            cloneOriginalProperties(originalNode, cloneNode);
+
+            if (isLastNode(originalNode)) {
+                break;
+            }
+            originalNode = originalNode.next;
+        }
+        return correspondingNodes.get(startNode);
+    }
+
+    protected void cloneOriginalProperties(Node originalNode, Node cloneNode) {
+        cloneNode.value = originalNode.value;
+        cloneOriginalNext(originalNode, cloneNode);
+        cloneOriginalArbitrary(originalNode, cloneNode);
+    }
+
+    protected void cloneOriginalNext(Node originalNode, Node cloneNode) {
+        if (originalNode.next != null) {
+            cloneNode.next = new Node();
+            correspondingNodes.put(originalNode.next, cloneNode.next);
+        }
+    }
+
+    protected void cloneOriginalArbitrary(Node originalNode, Node cloneNode) {
+        if (originalNode.arbitrary != null) {
+
+            if (correspondingNodes.get(originalNode.arbitrary) != null) {
+                // The corresponding clone node already exists. Use it.
+                cloneNode.arbitrary = correspondingNodes.get(originalNode.arbitrary);
+            } else {
+                cloneNode.arbitrary = new Node();
+                correspondingNodes.put(originalNode.arbitrary, cloneNode.arbitrary);
+            }
+        }
+    }
+
+    public Boolean isLastNode(Node node) {
+        return node.next == null;
+    }
 
 }
